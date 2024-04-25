3480539475eb00e389e291c8a15a0614b9a68369
pugazhenthi2002 <pugazhenthi2002@gmail.com>
2024-04-25 09:50:01 +0530

    TransparentForm

--- UserInterface/Form1.cs
+++ UserInterface/Form1.cs
@@ -12,17 +12,35 @@ using System.Windows.Forms.DataVisualization.Charting;
 using LiveCharts;
 using LiveCharts.Wpf;
 using TeamTracker;
-
-
+using UserInterface.ViewPage;
 
 namespace UserInterface
 {
     public partial class Form1 : Form
     {
-        private string prevString;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TransparencyForm form = new TransparencyForm();
+            form.Show();
+            WarningForm form1 = new WarningForm()
+            {
+                Content = "Hello"
+            };
+            form1.WarningStatus += OnWarningStatus;
+            form.ShowForm(form1);
+        }
+
+        private void OnWarningStatus(object sender, bool e)
+        {
+            if (e)
+            {
+
+            }
         }
     }
 }
--- UserInterface/TransparencyForm.Designer.cs
+++ UserInterface/TransparencyForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UserInterface
 {
-    partial class Form1
+    partial class TransparencyForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,36 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(548, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 54);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
+            // TransparencyForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1385, 514);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "e";
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TransparencyForm";
+            this.Text = "TransparencyForm";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private TeamTracker.RippleButton rippleButton1;
-        private System.Windows.Forms.Button button1;
     }
 }
-
