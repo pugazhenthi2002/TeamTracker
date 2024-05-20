48f876da6b35304701b6ae5c277c663eec78e4f1
ILAMUGUNTHAN <ilamugunthan.n2020cse@sece.ac.in>
2024-05-20 15:46:03 +0530

    *Issueinfoform

--- UserInterface/Issues/IssueInfoForm.cs
+++ UserInterface/Issues/IssueInfoForm.cs
@@ -26,7 +26,25 @@ namespace TeamTracker
             ThemeManager.ThemeChange += OnThemeChanged;
             solutionTextBox.GotFocus += RemoveSolutionPlaceHolders;
             solutionTextBox.LostFocus += AddSolutionPlaceHolders;
+            InitializeRoundedEdge();
         }
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+        private void InitializeRoundedEdge()
+        {
+            labelAttachment.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, labelAttachment.Width, labelAttachment.Height, labelAttachment.Width, labelAttachment.Height));
+        }
+
 
         private void OnThemeChanged(object sender, EventArgs e)
         {
@@ -275,15 +293,6 @@ namespace TeamTracker
             Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
         }
 
-        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
-        private static extern IntPtr CreateRoundRectRgn
-        (
-            int nLeftRect,     // x-coordinate of upper-left corner
-            int nTopRect,      // y-coordinate of upper-left corner
-            int nRightRect,    // x-coordinate of lower-right corner
-            int nBottomRect,   // y-coordinate of lower-right corner
-            int nWidthEllipse, // height of ellipse
-            int nHeightEllipse // width of ellipse
-        );
+        
     }
 }
--- UserInterface/LoaderForm.cs
+++ UserInterface/LoaderForm.cs
@@ -28,7 +28,6 @@ namespace UserInterface1
         public void Start()
         {
             
-
         }
     }
 }
