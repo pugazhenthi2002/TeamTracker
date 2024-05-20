7d3182c7aa19bdd6ae01616228bec62c5350944d
ILAMUGUNTHAN <ilamugunthan.n2020cse@sece.ac.in>
2024-05-20 15:22:54 +0530

    *loader added

---
+++ UserInterface/LoaderForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TeamTracker;
+
+namespace UserInterface1
+{
+    public partial class LoaderForm : Form
+    {
+        public LoaderForm()
+        {
+            InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            TransparencyKey = Color.FromArgb(255, 100, 100, 255);
+            BackColor = Color.FromArgb(1, 1, 1);
+            Opacity = 0.4;
+        }
+
+        public void Start()
+        {
+            
+
+        }
+    }
+}
